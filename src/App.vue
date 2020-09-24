bca208b67891d2697a06789e9e9836bdd3303dc1
georgematos <georgemattos@gmail.com>
2020-09-24 15:59:01 -0300

    list of todos improved with icon and action buttons

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="" id="app">
     <div class="container grid-xs py-2">
-      <img class="img-responsive img-logo" src="@/assets/logo.png" alt="logo do vue">
+      <img
+        class="img-responsive img-logo"
+        src="@/assets/logo.png"
+        alt="logo do vue"
+      />
       <form @submit.prevent="addTodo(todo)">
         <div class="input-group">
-          <input type="text" class="form-input" placeholder="Novo todo" v-model="todo.description">
-          <button class='btn btn-primary input-group-btn'>Adicionar</button>
+          <input
+            type="text"
+            class="form-input"
+            placeholder="Novo todo"
+            v-model="todo.description"
+          />
+          <button class="btn btn-primary input-group-btn">Adicionar</button>
         </div>
       </form>
-      <div>
-        <ol>
-          <li v-for="todo in todos" :key="todo.id">
-            {{ todo.id }} - {{ todo.description }} -> {{todo.checked}}
-          </li>
-        </ol>
+      <div class="todo-list">
+        <div class="tile flex-centered" v-for="todo in todos" :key="todo.id">
+          <div class="tile-icon">
+            <i class="icon icon-time centered"></i>
+          </div>
+          <div class="tile-content">
+            <div class="tile-subtitle">{{todo.description}}</div>
+          </div>
+          <div class="tile-action">
+            <button class="btn btn-link">Concluir</button>
+            <button class="btn btn-link">
+              <span class="text-error">Remover</span>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -26,26 +44,29 @@ export default {
       todos: [],
       todo: {
         id: 0,
-        description: '',
-        checked: false
-      }
-    }
+        description: "",
+        checked: false,
+      },
+    };
   },
   methods: {
     addTodo() {
       this.todos.push({
         id: Date.now(),
         description: this.todo.description,
-        checked: false
-      })
-    }
-  }
-}
+        checked: false,
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .img-logo {
-    max-width: 200px;
-    margin: 0 auto;
-  }
+.img-logo {
+  max-width: 200px;
+  margin: 0 auto;
+}
+.todo-list {
+  padding-top: 2rem;
+}
 </style>
